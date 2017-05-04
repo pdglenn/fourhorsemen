fe6e4f2ae0664d3869a85fb9ac06a2776fc1dba7
Paul Glenn <paul.glenn@gmail.com>
2017-05-04 16:39:33 -0700

    Changed next button from class to id in experience.js

--- fourhorsemen/fourhorsemen/static/js/experience.js
+++ fourhorsemen/fourhorsemen/static/js/experience.js
@@ -1,6 +1,6 @@
 function revealBottomContent(){
     $('.bottom_content').css('display', 'inherit');
-    $('.next_button').removeClass('last');
+    $('#next_button').removeClass('last');
     var aTag = $("a[name='"+ "bottom_content" +"']");
     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
     
