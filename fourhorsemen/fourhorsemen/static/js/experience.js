ce0b916377075dbcedcf924777401679e34e96c1
Paul Glenn <paul.glenn@gmail.com>
2017-05-03 23:07:39 -0700

    Added logic to hide bottom of page and reveal on button to content_assessment

---
+++ fourhorsemen/fourhorsemen/static/js/experience.js
@@ -0,0 +1,7 @@
+function revealBottomContent(){
+    $('.bottom_content').css('display', 'inherit');
+    $('#next_button').removeClass('last');
+    var aTag = $("a[name='"+ "bottom_content" +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+
+}
