0ce40b5e9efd3924a0be060d9014a4272c098548
Paul Glenn <paul.glenn@gmail.com>
2017-05-11 14:54:13 -0700

    Added user input responsiveness to context_matters

--- fourhorsemen/static/js/context_matters.js
+++ fourhorsemen/static/js/context_matters.js
@@ -1,5 +1,17 @@
 function contextMatters(){
     var chartDiv = '#chart'
-    bar_chart(chartDiv, {"x": 'Instagram', "y": "0"}, data);
+
+    var userChoice = $('input:checked').val();
+    console.log('hi')
+    var chartDiv = '#chart'
+
+    choiceMap = {'Not harassing': '1',
+                 'Slightly harassing': '2',
+                 'Somewhat harassing': '3',
+                 'Very harassing': '4',
+                 'Extremely harassing': '5'}
+     choiceNum = choiceMap[userChoice]
+
+    bar_chart(chartDiv, {"x": choiceNum, "y": "0"}, data);
     revealBottomContent();
 }
