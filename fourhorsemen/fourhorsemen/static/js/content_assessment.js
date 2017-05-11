5c677a7c7b0a24e8ca6990c43929024b367981f8
Paul Glenn <paul.glenn@gmail.com>
2017-05-10 23:55:50 -0700

    added a 1 to the end of the bar chart call in content_assessment per jason's request

--- fourhorsemen/fourhorsemen/static/js/content_assessment.js
+++ fourhorsemen/fourhorsemen/static/js/content_assessment.js
@@ -13,6 +13,6 @@ function contentAssessment(){
 
             one_axis_scatter(chartDiv,{"x": userChoice, "y": "0", "r": "30"},harassment_distribution);
         line_plot(chartDiv,{"x": choiceNum},harassment_distribution_line);
-        bar_chart_percent(chartDiv,{"x": userChoice, "y": "0"},harassment_distribution_bar)
+        bar_chart_percent(chartDiv,{"x": userChoice, "y": "0"},harassment_distribution_bar, 1)
     revealBottomContent();
 }
