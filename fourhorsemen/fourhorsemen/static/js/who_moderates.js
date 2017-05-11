49cc4e6b3644bf06c001604f4be11ef6e220adde
mollymahar <mollymahar@gmail.com>
2017-05-11 15:43:22 -0700

    minor changes

--- fourhorsemen/fourhorsemen/static/js/who_moderates.js
+++ fourhorsemen/fourhorsemen/static/js/who_moderates.js
@@ -2,8 +2,8 @@ function whoModerates(userChoice){
     var chartDiv1 = '#chart1'
     var chartDiv2 = '#chart2'
     var chartDiv3 = '#chart3'
-    bar_chart(chartDiv1,{"x": "Users", "y": ""},fair,3)
-    bar_chart(chartDiv2,{"x": "Users", "y": ""},accurate,3)
-    bar_chart(chartDiv3,{"x": "Users", "y": ""},trust,3)
+    bar_chart(chartDiv1,{"x": "Users", "y": ""},fair,3, 300, 400)
+    bar_chart(chartDiv2,{"x": "Users", "y": ""},accurate,3, 300, 400)
+    bar_chart(chartDiv3,{"x": "Users", "y": ""},trust,3, 300, 400)
     revealBottomContent();
 }
